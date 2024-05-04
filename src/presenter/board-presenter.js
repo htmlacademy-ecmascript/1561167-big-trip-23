d71988596f88d22d271a1d9382c054f1aa13d6f8
Kholodov <kholodov.vadym@yandex.ru>
2024-05-04 17:54:35 +0300

    Добавит классам приватные поля и геттеры

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,36 +6,41 @@ import SortView from '../view/sort-view';
 import { render } from '../framework/render';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  pointListComponent = new PointListView();
+  #boardContainer = null;
+  #tripModel = null;
+
+  #boardComponent = new BoardView();
+  #pointListComponent = new PointListView();
+
+  #boardPoints = null;
 
   constructor({ boardContainer, tripModel }) {
-    this.boardContainer = boardContainer;
-    this.tripModel = tripModel;
+    this.#boardContainer = boardContainer;
+    this.#tripModel = tripModel;
   }
 
   init = () => {
-    this.boardPoints = [...this.tripModel.points];
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.element);
-    render(this.pointListComponent, this.boardComponent.element);
+    this.#boardPoints = [...this.#tripModel.points];
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#pointListComponent, this.#boardComponent.element);
 
     render(
       new PointEditView({
-        point: this.boardPoints[11],
-        destinations: this.tripModel.destinations,
-        offers: this.tripModel.offers,
+        point: this.#boardPoints[11],
+        destinations: this.#tripModel.destinations,
+        offers: this.#tripModel.offers,
       }),
-      this.pointListComponent.element
+      this.#pointListComponent.element
     );
-    for (let i = 1; i < this.boardPoints.length; i++) {
+    for (let i = 1; i < this.#boardPoints.length; i++) {
       render(
         new PointView({
-          point: this.boardPoints[i],
-          destinations: this.tripModel.destinations,
-          offers: this.tripModel.offers,
+          point: this.#boardPoints[i],
+          destinations: this.#tripModel.destinations,
+          offers: this.#tripModel.offers,
         }),
-        this.pointListComponent.element
+        this.#pointListComponent.element
       );
     }
   };
